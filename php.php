016dfa3b280cd409b6f6d5a965164dad017e8343
aliriocerafica <127605167+aliriocerafica@users.noreply.github.com>
2023-11-13 23:55:08 +0800

    Update php.php

--- php.php
+++ php.php
@@ -86,6 +86,7 @@
                     <option value="Tuesday">Tuesday</option>
                     <option value="Wednesday">Wednesday</option>
                     <option value="Thursday">Thursday</option>
+                     <option value="Friday">Friday</option>
                     <option value="Saturday">Saturday</option>
                     <option value="Sunday">Sunday</option>
                 </select>
